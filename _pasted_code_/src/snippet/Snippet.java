d77c58fea6f0402597985420e9c3261efd926766
wonjinnn <won98jin@naver.com>
2022-04-01 12:53:42 +0900

    123
    123

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,9 @@
+package snippet;
+
+public class Snippet {
+	Invalid project description.
+	Invalid project description.
+	OK
+	C:\Users\human\Desktop\team\teamproject overlaps the workspace location: C:\Users\human\Desktop\team\teamproject
+}
+
